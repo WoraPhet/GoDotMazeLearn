09babb2291ca3c3ecf6f72d21d7126ff19a36c1e
sitthatot <sitthatot@gmail.com>
2023-08-30 18:08:45 +0700

    add health bar but not function

---
+++ monster_1.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+var speed = 100
+var direction = 1 #right
+
+func _physics_process(delta):
+	if is_on_wall():
+		direction = direction * -1
+		$AnimatedSprite2D.flip_h = not $AnimatedSprite2D.flip_h
+	position.x += speed * delta
+	move_and_slide()
